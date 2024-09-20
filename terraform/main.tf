c4b06017af0dcdecfa93d01c240efcf43d62cfe6
Denys Vasyliev <den.vasyliev@gmail.com>
2024-09-20 03:28:38 +0100

    aff flux to main

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,3 +86,28 @@ resource "google_container_node_pool" "gpu_pool" {
     }
   }
 }
+
+# ==========================================
+# Initialise a Github project
+# ==========================================
+
+resource "github_repository" "this" {
+  name        = var.github_repository
+  description = var.github_repository
+  visibility  = "private"
+  auto_init   = true
+
+  # Enable vulnerability alerts
+  vulnerability_alerts = true
+}
+
+# ==========================================
+# Bootstrap Flux
+# ==========================================
+
+resource "flux_bootstrap_git" "this" {
+  depends_on = [github_repository.this]
+
+  embedded_manifests = true
+  path               = "clusters/$${cluster.name}"
+}
