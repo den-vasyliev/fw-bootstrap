864af463497a47505248bf594d813f26d284283e
Denys Vasyliev <den.vasyliev@gmail.com>
2024-09-20 04:13:12 +0100

    rm repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,23 +91,23 @@ resource "google_container_node_pool" "gpu_pool" {
 # Initialise a Github project
 # ==========================================
 
-resource "github_repository" "this" {
-  name        = var.github_repository
-  description = var.github_repository
-  visibility  = "private"
-  auto_init   = true
+# resource "github_repository" "this" {
+#   name        = var.github_repository
+#   description = var.github_repository
+#   visibility  = "private"
+#   auto_init   = true
 
-  # Enable vulnerability alerts
-  vulnerability_alerts = true
-}
+#   # Enable vulnerability alerts
+#   vulnerability_alerts = true
+# }
 
 # ==========================================
 # Bootstrap Flux
 # ==========================================
 
-resource "flux_bootstrap_git" "this" {
-  depends_on = [github_repository.this]
+# resource "flux_bootstrap_git" "this" {
+#   depends_on = [github_repository.this]
 
-  embedded_manifests = true
-  path               = "clusters"
-}
+#   embedded_manifests = true
+#   path               = "clusters"
+# }
